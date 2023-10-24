837c719a4390907a1ad0ff37f75ad75c2462ffe6
Omar <OmarAbdulla7@hotmail.com>
2023-10-24 16:18:52 +0300

    Print the constructed manifest in kotlin

--- examples/transactions/construction-of-simple-transaction/Kotlin/src/main/kotlin/Main.kt
+++ examples/transactions/construction-of-simple-transaction/Kotlin/src/main/kotlin/Main.kt
@@ -15,6 +15,7 @@ fun main(args: Array<String>) {
 
     // Constructing the manifest
     val manifest = ManifestBuilder().faucetLockFee().dropAllProofs().build(networkId)
+    println("Constructed manifest: ${manifest.instructions().asStr()}");
 
     // Constructing the transaction
     val currentEpoch = MockGatewayApiClient.currentEpoch()
