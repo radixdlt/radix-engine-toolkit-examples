ff9ebb910bf340300b69e774f99760ef4454cfa3
Omar <OmarAbdulla7@hotmail.com>
2024-04-17 18:57:07 +0300

    add tombstone

--- examples/transactions/batch-transfers-from-csv/Python/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-
-python -m pip install -r $SCRIPT_DIR/requirements.txt > /dev/null 2>&1
-python main.py
